c4cc3fe30800ea5a33ab5842195786633024e15d
musabbirsammak <musabbirhasansammak@gmail.com>
2022-03-25 13:50:49 +0600

    setupLibraries.R
    Separates installations of CRAN and Bioconductor packages.

--- setupLibraries.R
+++ setupLibraries.R
@@ -1,6 +1,11 @@
-list.of.packages <- c("dplyr", "plyr", "stringr", "TCGAbiolinks", "SummarizedExperiment", 
-                      "ggplot2", "ggpubr", "DESeq2", "DEGreport", "apeglm",
-                      "EnhancedVolcano", "enrichplot")
-
+list.of.packages <- c("dplyr", "plyr", "stringr", "ggplot2", "ggpubr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+list.of.bioconductor.packages <- c("TCGAbiolinks", "SummarizedExperiment", 
+                                   "DESeq2", "DEGreport", "apeglm",
+                                   "EnhancedVolcano", "enrichplot")
+new.bioconductor.packages <- list.of.bioconductor.packages[!(list.of.bioconductor.packages %in% installed.packages()[,"Package"])]
+if(length(new.bioconductor.packages)) BiocManager::install(new.bioconductor.packages)
