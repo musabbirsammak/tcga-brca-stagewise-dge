c95ce884182dee8ee9f1618f7eb4b3610bfc8a4e
musabbirsammak <musabbirhasansammak@gmail.com>
2022-10-05 09:24:50 +0600

    Reorganizes getData.R.
    workflow.type of GDCQuery is changed to 'STAR - Counts' from 'HTSeq - Counts' as previous one was not working.
    
    Added codes for downloading XML clinical data as they contain more information.

--- getData.R
+++ getData.R
@@ -10,9 +10,22 @@ if (!dir.exists('Data')) {
 query.exp <- GDCquery(project = "TCGA-BRCA",
                       data.category = "Transcriptome Profiling",
                       data.type = "Gene Expression Quantification", 
-                      workflow.type = "HTSeq - Counts")
+                      workflow.type = "STAR - Counts")
 GDCdownload(query.exp)
 dataExpression <- GDCprepare(query.exp)
+
+# Downloads patient, drug, and radiation data
+query.clin <- GDCquery(project = "TCGA-BRCA",
+                       data.category = "Clinical",
+                       file.type = "xml") 
+GDCdownload(query.clin)
+clinical.patient <- GDCprepare_clinic(query.clin, clinical.info = "patient")
+clinical.drug <- GDCprepare_clinic(query.clin, clinical.info = "drug")
+clinical.radiation <- GDCprepare_clinic(query.clin, clinical.info = "radiation")
+
+write.csv(clinical.patient, './Data/clinical_patient.csv')
+write.csv(clinical.drug, './Data/clinical_drug.csv')
+write.csv(clinical.radiation, './Data/clinical_radiation.csv')
 
 # Separates clinical, expression, and annotation data
 clinicalData <- as.data.frame(colData(dataExpression))
